03d86ae0e761c394475cfbaf5642ee4ccd0a1b95
miguelako029 <miguellmilanez@gmail.com>
2023-08-22 10:32:54 +0800

    updated experience page

--- src/pages/experience.jsx
+++ src/pages/experience.jsx
@@ -4,9 +4,15 @@ import React, { useState } from "react";
 import { Box } from "@mui/material";
 import { useColorMode } from "../assets/script/DarkModeContext"; // Update the path
 import "../styles/style.css";
+import Button from "@mui/material/Button";
+import Racami from "../assets/images/racami.png";
+import Strategist from "../assets/images/strategist.png";
+import Indra from "../assets/images/indra.png";
+
+import { BorderColor } from "@mui/icons-material";
 
 const Experience = () => {
-  const { mode, backgroundColor, fontColor } = useColorMode();
+  const { mode, backgroundColor, fontColor, BorderColor } = useColorMode();
   return (
     <Box
       className="pageSection"
@@ -28,6 +34,62 @@ const Experience = () => {
           <div className="labelContainer">
             <h1>Experience</h1>
           </div>
+          <section class="layout">
+            <div class="cardBox">
+              <div class="card">
+                <div class="circled-number">1</div>
+                <div class="years">2022 - 2023</div>
+                <img class="card-image" src={Racami} alt="Card Image" />
+                <Button
+                  className="read-more"
+                  sx={{
+                    background: backgroundColor,
+                    color: fontColor,
+                    border: BorderColor,
+                    transition: "background-color 0.7s ease",
+                  }}
+                >
+                  Read More
+                </Button>
+              </div>
+            </div>
+            <div class="cardBox">
+              <div class="card">
+                <div class="circled-number">2</div>
+                <div class="years">2022 - 2023</div>
+                <img class="card-image" src={Strategist} alt="Card Image" />
+                <Button
+                  className="read-more"
+                  sx={{
+                    background: backgroundColor,
+                    color: fontColor,
+                    border: BorderColor,
+                    transition: "background-color 0.7s ease",
+                  }}
+                >
+                  Read More
+                </Button>
+              </div>
+            </div>
+            <div class="cardBox">
+              <div class="card">
+                <div class="circled-number">3</div>
+                <div class="years">2022 - 2023</div>
+                <img class="card-image" src={Indra} alt="Card Image" />
+                <Button
+                  className="read-more"
+                  sx={{
+                    background: backgroundColor,
+                    color: fontColor,
+                    border: BorderColor,
+                    transition: "background-color 0.7s ease",
+                  }}
+                >
+                  Read More
+                </Button>
+              </div>
+            </div>
+          </section>
         </div>
       </div>
     </Box>
