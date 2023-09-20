3ee2b050c91e9293a429921b9f955dae24d96f01
miguelako029 <miguellmilanez@gmail.com>
2023-09-20 08:51:03 +0800

    complete

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -20,6 +20,9 @@ import {
   Send,
 } from "@mui/icons-material";
 
+import { useSpring, animated } from "react-spring";
+import { useInView } from "react-intersection-observer";
+
 const Contact = () => {
   const { mode, backgroundColor, fontColor, BorderColor } = useColorMode();
 
@@ -43,6 +46,25 @@ const Contact = () => {
       },
     },
   };
+
+  const [ref, inView] = useInView({
+    triggerOnce: true, // Only trigger once when it enters the viewport
+  });
+
+  // Define the animation properties
+  const fadeInUp = useSpring({
+    from: {
+      opacity: 0,
+      transform: "translateY(50px)",
+    },
+    to: {
+      opacity: inView ? 1 : 0, // Animate opacity when in view
+      transform: inView ? "translateY(0)" : "translateY(30px)", // Animate translateY when in view
+    },
+    config: {
+      duration: 1000,
+    },
+  });
 
   return (
     <Box
@@ -68,73 +90,80 @@ const Contact = () => {
               maxWidth="md"
               style={{ textAlign: "center", marginTop: "50px" }}
             >
-              <Typography variant="h3" gutterBottom>
-                Let's get connected
-              </Typography>
-              <form noValidate autoComplete="off">
-                {/* Apply the style to TextField */}
-                <TextField
-                  id="email"
-                  label="Email"
-                  variant="outlined"
-                  fullWidth
-                  margin="normal"
-                  sx={textFieldStyle}
-                />
-                <Button
-                  sx={{
-                    background: backgroundColor,
-                    color: fontColor,
-                    border: "1px solid",
-                    transition: "background-color 0.7s ease",
-                  }}
-                  className="send"
-                  type="submit"
-                  style={{ marginTop: "10px" }}
-                >
-                  Send
-                  <Send sx={{ fontSize: 20, margin: "0px 0px 0px 10px" }} />
-                </Button>
-                <div style={{ paddingTop: "200px" }}>
-                  {/* Social media icons */}
+              <div ref={ref}>
+                <animated.div style={fadeInUp}>
+                  <Typography variant="h3" gutterBottom>
+                    Let's get connected
+                  </Typography>
+                  <form noValidate autoComplete="off">
+                    {/* Apply the style to TextField */}
+                    <TextField
+                      id="email"
+                      label="Email"
+                      variant="outlined"
+                      fullWidth
+                      margin="normal"
+                      sx={textFieldStyle}
+                    />
+                    <Button
+                      sx={{
+                        background: backgroundColor,
+                        color: fontColor,
+                        border: "1px solid",
+                        transition: "background-color 0.7s ease",
+                      }}
+                      className="send"
+                      type="submit"
+                      style={{ marginTop: "10px" }}
+                    >
+                      Send
+                      <Send sx={{ fontSize: 20, margin: "0px 0px 0px 10px" }} />
+                    </Button>
+                    <div style={{ paddingTop: "200px" }}>
+                      {/* Social media icons */}
 
-                  <a
-                    href="https://www.linkedin.com/in/miguel-lorenzo-mila%C3%B1ez-706482144/"
-                    target="blank"
-                  >
-                    <Facebook id="contactIcon" sx={{ color: fontColor }} />
-                  </a>
-                  <a
-                    href="https://www.linkedin.com/in/miguel-lorenzo-mila%C3%B1ez-706482144/"
-                    target="blank"
-                  >
-                    <Twitter id="contactIcon" sx={{ color: fontColor }} />
-                  </a>
-                  <a
-                    href="https://www.linkedin.com/in/miguel-lorenzo-mila%C3%B1ez-706482144/"
-                    target="blank"
-                  >
-                    <Instagram id="contactIcon" sx={{ color: fontColor }} />
-                  </a>
-                  <a
-                    href="https://www.linkedin.com/in/miguel-lorenzo-mila%C3%B1ez-706482144/"
-                    target="blank"
-                  >
-                    <LinkedIn id="contactIcon" sx={{ color: fontColor }} />
-                  </a>
-                  <a
-                    href="https://www.linkedin.com/in/miguel-lorenzo-mila%C3%B1ez-706482144/"
-                    target="blank"
-                  >
-                    <GitHub id="contactIcon" sx={{ color: fontColor }} />
-                  </a>
-                  <Tooltip title="miguellmilanez@gmail.com" placement="top">
-                    <Email id="contactIcon" sx={{ color: fontColor }} />
-                  </Tooltip>
-                </div>
-              </form>
+                      <a
+                        href="https://www.linkedin.com/in/miguel-lorenzo-mila%C3%B1ez-706482144/"
+                        target="blank"
+                      >
+                        <Facebook id="contactIcon" sx={{ color: fontColor }} />
+                      </a>
+                      <a
+                        href="https://www.linkedin.com/in/miguel-lorenzo-mila%C3%B1ez-706482144/"
+                        target="blank"
+                      >
+                        <Twitter id="contactIcon" sx={{ color: fontColor }} />
+                      </a>
+                      <a
+                        href="https://www.linkedin.com/in/miguel-lorenzo-mila%C3%B1ez-706482144/"
+                        target="blank"
+                      >
+                        <Instagram id="contactIcon" sx={{ color: fontColor }} />
+                      </a>
+                      <a
+                        href="https://www.linkedin.com/in/miguel-lorenzo-mila%C3%B1ez-706482144/"
+                        target="blank"
+                      >
+                        <LinkedIn id="contactIcon" sx={{ color: fontColor }} />
+                      </a>
+                      <a
+                        href="https://www.linkedin.com/in/miguel-lorenzo-mila%C3%B1ez-706482144/"
+                        target="blank"
+                      >
+                        <GitHub id="contactIcon" sx={{ color: fontColor }} />
+                      </a>
+                      <Tooltip title="miguellmilanez@gmail.com" placement="top">
+                        <Email id="contactIcon" sx={{ color: fontColor }} />
+                      </Tooltip>
+                    </div>
+                  </form>
 
-              <p>&copy; 2023 Miguel Lorenzo Milanez. All rights reserved.</p>
+                  <p>
+                    &copy; {new Date().getFullYear()} Miguel Lorenzo Milanez.
+                    All rights reserved.
+                  </p>
+                </animated.div>
+              </div>
             </Container>
           </div>
         </div>
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -7,8 +7,30 @@ import "../styles/style.css";
 import ImageGallery from "../components/imageGallery";
 import image from "../assets/images/logo192.png";
 
+import { useSpring, animated } from "react-spring";
+import { useInView } from "react-intersection-observer";
+
 const Portfolio = () => {
   const { mode, backgroundColor, fontColor } = useColorMode();
+
+  const [ref, inView] = useInView({
+    triggerOnce: true, // Only trigger once when it enters the viewport
+  });
+
+  // Define the animation properties
+  const fadeInUp = useSpring({
+    from: {
+      opacity: 0,
+      transform: "translateY(50px)",
+    },
+    to: {
+      opacity: inView ? 1 : 0, // Animate opacity when in view
+      transform: inView ? "translateY(0)" : "translateY(30px)", // Animate translateY when in view
+    },
+    config: {
+      duration: 1000,
+    },
+  });
 
   return (
     <Box
@@ -31,9 +53,14 @@ const Portfolio = () => {
       <div className="pageSection" id="portfolio">
         <div className="portfolioContainer">
           <div className="labelContainer">
-            <h1 className="port-h1">Portfolio</h1>
+            <div ref={ref}>
+              <animated.div style={fadeInUp}>
+                <h1 className="port-h1">Portfolio</h1>
+
+                <ImageGallery />
+              </animated.div>
+            </div>
           </div>
-          <ImageGallery />
         </div>
       </div>
     </Box>
