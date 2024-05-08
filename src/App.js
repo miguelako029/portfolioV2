8d2fee17bd8995637b26f9e6d30b2577393fb3f2
miguelako029 <miguellmilanez@gmail.com>
2024-05-08 20:32:59 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
     <ColorModeProvider>
       <ThemeProvider theme={theme}>
         <NavBar />
-        {/* <Hero /> */}
+        <Hero />
         <About />
         <Experience />
         <Portfolio />
