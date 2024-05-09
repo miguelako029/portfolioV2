c098a42265e1cb7cd4d31ad54a6d83bc797f783c
miguelako029 <miguellmilanez@gmail.com>
2024-05-09 08:53:52 +0800

    updated

--- src/components/projectGallery/portfolio.js
+++ src/components/projectGallery/portfolio.js
@@ -1,1 +1,100 @@
-<p>test</p>;
+import React, { useState } from "react";
+import { useColorMode } from "../../assets/script/DarkModeContext"; // Update the path
+
+import PropTypes from "prop-types";
+import SwipeableViews from "react-swipeable-views";
+// import { useTheme } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+function PortfolioTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="PortfolioTabPanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+PortfolioTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`,
+  };
+}
+
+export default function FullWidthTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
+
+  const { mode, backgroundColor, fontColor, borderColor } = useColorMode(); // Update variable names
+
+  return (
+    <Box sx={{ bgcolor: "background.paper", height: "100%" }}>
+      <Box className="containers">
+        <Box className="content">
+          <SwipeableViews
+            axis={"rtl" ? "x-reverse" : "x"}
+            index={value}
+            onChangeIndex={handleChangeIndex}
+          >
+            <PortfolioTabPanel value={value} index={0}>
+              Item One
+            </PortfolioTabPanel>
+            <PortfolioTabPanel value={value} index={1}>
+              Item Two
+            </PortfolioTabPanel>
+            <PortfolioTabPanel value={value} index={2}>
+              Item Three
+            </PortfolioTabPanel>
+          </SwipeableViews>
+        </Box>
+        <AppBar
+          position="static"
+          className="tabContainer"
+          sx={{ color: fontColor }} // Utilize fontColor here
+        >
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            indicatorColor="secondary"
+            textColor="inherit"
+            variant="fullWidth"
+            aria-label="full width tabs example"
+          >
+            <Tab label="Item One" {...a11yProps(0)} />
+            <Tab label="Item Two" {...a11yProps(1)} />
+            <Tab label="Item Three" {...a11yProps(2)} />
+          </Tabs>
+        </AppBar>
+      </Box>
+    </Box>
+  );
+}
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -5,68 +5,39 @@ import { Box } from "@mui/material";
 import { useColorMode } from "../assets/script/DarkModeContext"; // Update the path
 import "../styles/style.css";
 import ImageGallery from "../components/imageGallery";
-import myWorks from "../components/projectGallery/portfolio";
+import Portfolio from "../components/projectGallery/portfolio";
 // import image from "../assets/images/logo192.png";
 
 import { useSpring, animated } from "react-spring";
 import { useInView } from "react-intersection-observer";
 
-const Portfolio = () => {
+const PortfolioPage = () => {
   const { mode, backgroundColor, fontColor } = useColorMode();
 
   const [ref, inView] = useInView({
     triggerOnce: true, // Only trigger once when it enters the viewport
   });
 
-  // Define the animation properties
-  const fadeInUp = useSpring({
-    from: {
-      opacity: 0,
-      transform: "translateY(50px)",
-    },
-    to: {
-      opacity: inView ? 1 : 0, // Animate opacity when in view
-      transform: inView ? "translateY(0)" : "translateY(30px)", // Animate translateY when in view
-    },
-    config: {
-      duration: 1000,
-    },
-  });
-
   return (
     <Box
-      className="pageSection"
-      id="portfolio"
       sx={{
         background: backgroundColor,
         color: fontColor,
-        minHeight: "80vh",
-        paddingTop: "150px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        textAlign: "center",
-        flexDirection: "column",
+
         transition: "background-color 0.7s ease",
         // width: "80vw",
       }}
     >
-      <div className="pageSection" id="portfolio">
-        <div className="portfolioContainer">
-          <div className="labelContainer">
-            <div ref={ref}>
-              <animated.div style={fadeInUp}>
-                <h1 className="port-h1">Portfolio</h1>
-                <myWorks />
+      <div className="portfolioContainer">
+        <div ref={ref}>
+          <h1 className="port-h1">Portfolio</h1>
+          <Portfolio />
 
-                {/* <ImageGallery /> */}
-              </animated.div>
-            </div>
-          </div>
+          {/* <ImageGallery /> */}
         </div>
       </div>
     </Box>
   );
 };
 
-export default Portfolio;
+export default PortfolioPage;
